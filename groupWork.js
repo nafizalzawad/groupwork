dbdbfc90c48341146df6ee9b63a43f214dc98e7c
Touhidul islam <touhidulislam066@gmail.com>
2025-12-07 18:44:07 +0300

    Add Member Object By Touhedul

--- groupWork.js
+++ groupWork.js
@@ -1,7 +1,34 @@
 // টিম মেম্বারদের নামের অ্যারে
-const teamMembers = ['Rafi', 'Meem', 'Shuvo'];
+const teamMembers = ['Nafiz', 'Millat', 'Touheul'];
 
 // স্কোর ভেরিয়েবল
-let rafiScore = 0;
-let meemScore = 0;
-let shuvoScore = 0;
+let NafizScore = 0;
+let MillatScore = 0;
+let TouhedulScore = 0;
+
+const Nafiz = {
+  id: Symbol('Nafiz'),
+  name: 'Nafiz',
+  age: 23,
+  isActive: true,
+  bugs: ['UI Bug', 'API Bug', 'Login Bug'],
+  bugCount: 3
+};
+
+const Millat = {
+  id: Symbol('Millat'),
+  name: 'Millat',
+  age: 21,
+  isActive: true,
+  bugs: ['UI Bug'],
+  bugCount: 1
+};
+
+const Touhedul = {
+  id: Symbol('Touhedul'),
+  name: 'Touhedul',
+  age: 25,
+  isActive: true,
+  bugs: ['UI Bug', 'API Bug', 'Logic Bug', 'DB Bug', 'Crash Bug', 'Auth Bug'],
+  bugCount: 6
+};
